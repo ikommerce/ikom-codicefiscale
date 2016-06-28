2e9942e78f9e74182516e8e3e5023e8629a75286
Maria Branca <m.branca@ikom.it>
2016-06-28 11:52:27 +0200

    When the shipping country is Italy, and it is other than the billing country, fiscal code is mandatory

--- src/js/ikom/cf.js
+++ src/js/ikom/cf.js
@@ -1,8 +1,8 @@
 jQuery(function($) {
 	$('#billing\\:country_id').change(function(e) {
 		var $this = $(this);
-		var $input = $('#billing\\:vat_id');
-		var $label = $('label[for="billing:vat_id"]');
+		var $input = $('#checkout-step-billing #billing\\:vat_id');
+		var $label = $('#checkout-step-billing label[for="billing:vat_id"]');
 		if ($this.val() == "IT") {
 			$input.addClass('required-entry validate-alphanum');
 			$label.addClass('required');
@@ -13,6 +13,23 @@ jQuery(function($) {
 			$('#vat_id_required').remove();
 		}
 	}).trigger('change');
+	
+	/*Inserire il codice fiscale durante lo shipping se non è stato inserito durante il billing*/
+	$('#shipping\\:country_id').change(function(e){
+		var $this = $(this);
+		var $input = $('#checkout-step-shipping #billing\\:vat_id');
+		var $label = $('#checkout-step-shipping label[for="billing:vat_id"]');
+		if ($this.val() == "IT" && $('#billing\\:vat_id').val() == "") {
+			$input.addClass('required-entry validate-alphanum');
+			$label.addClass('required');
+			$('<span id="vat_id_required"><em>*</em></span>').appendTo($label);
+		} else {
+			$input.removeClass('required-entry validate-alphanum');
+			$label.removeClass('required');
+			$('#vat_id_required').remove();
+		}
+	}).trigger('change');
+	
 	
 	$('#country').change(function(e) {
 		var $this = $(this);
